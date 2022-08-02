51d40a685b887caf7abda6b3489776a7d1b0bc60
Hrafn Thorvaldsson <hrafn@hth.is>
2022-08-02 12:20:18 +0000

    - removed Spotless as it was not being used - added nexus publishing plugin - formatting

--- retrofit-result-adapter/build.gradle.kts
+++ retrofit-result-adapter/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.21"
-    alias(libs.plugins.spotless)
+    alias(libs.plugins.nexus)
 }
 
 group = "is.hth"
