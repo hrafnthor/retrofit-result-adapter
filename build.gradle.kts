da0b5c9bb563211e83509ed04c2b3c7a0226d294
Hrafn Þorvaldsson <hrafn@hth.is>
2022-09-22 13:53:58 +0000

    change: jvm compatibility set to version 8, down from 11 (#7)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,13 @@ allprojects {
     }
     tasks.withType(JavaCompile::class).configureEach {
         options.encoding = "UTF-8"
-        sourceCompatibility = JavaVersion.VERSION_11.toString()
-        targetCompatibility = JavaVersion.VERSION_11.toString()
+        sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+        targetCompatibility = JavaVersion.VERSION_1_8.toString()
     }
     tasks.withType(KotlinCompile::class).configureEach {
         kotlinOptions {
             // Set JVM target to 11
-            jvmTarget = JavaVersion.VERSION_11.toString()
+            jvmTarget = JavaVersion.VERSION_1_8.toString()
         }
     }
 }
